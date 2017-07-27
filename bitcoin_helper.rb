955417d86a25f687d0ae9101574bf98ef391cb4f
Ryan Kelley <rkelley@raysbaseball.com>
2017-07-27 00:44:51 -0400

    parsing yaml file and working

--- bitcoin_helper.rb
+++ bitcoin_helper.rb
@@ -1,18 +1,23 @@
 require 'yaml'
 
-language_data = YAML.load_file( File.join(File.dirname(File.expand_path(__FILE__)), 'language.yml' )
+language_data =
+  YAML.load_file(
+    File.join(
+      File.dirname(
+        File.expand_path(__FILE__) ), 'language.yml' ))
 
-say_no = language_data[:say_no]
-say_yes = language_data[:say_yes]
+say_no = language_data[ 'say_no' ]
+say_yes = language_data[ 'say_yes' ]
+prompt = "Ask me whether you should buy or sell Bitcoin. \n\n"
 
-print "Ask me if you should buy or sell Bitcoin. \n\n"
+print prompt
 
 until ( user_input = gets.chomp.strip ) =~ /(?:ex|qu)it/ix
 	input_is_ques = user_input[ -1 ].eql? '?'
 
   if user_input =~ /^.*(sell.*bit\ coin|sell.*bitcoin).*$/ix && input_is_ques
   	puts
-    puts "#{ say_no[ rand( say_no.length )]}, you should NOT sell Bitcoin. You should buy bitcoin"
+    puts "#{ say_no[ rand( say_no.length )]}, you should NOT sell Bitcoin. You should buy bitcoin."
   elsif user_input =~ /^.*(buy.*bit\ coin|buy.*bitcoin).*$/ && input_is_ques
   	puts
     puts "#{ say_yes[ rand( say_yes.length ) ] }, you should definitely buy Bitcoin."
@@ -20,6 +25,6 @@ until ( user_input = gets.chomp.strip ) =~ /(?:ex|qu)it/ix
   	puts
     puts "Please ask a question about buying or selling Bitcoin, and end it with '?'"
   end
-  print "Ask me if you should sell you Bitcoin. \n\n"
+  print prompt
 end
 
