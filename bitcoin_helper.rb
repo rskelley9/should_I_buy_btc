5549986e63a1a0c48e2ae34da52dbb32605fdb78
Ryan Kelley <rkelley@raysbaseball.com>
2017-07-27 02:00:06 -0400

    added another regex catching an or question, formatted responses

--- bitcoin_helper.rb
+++ bitcoin_helper.rb
@@ -1,30 +1,45 @@
 require 'yaml'
 
-language_data =
+@language_data =
   YAML.load_file(
     File.join(
       File.dirname(
         File.expand_path(__FILE__) ), 'language.yml' ))
 
-say_no = language_data[ 'say_no' ]
-say_yes = language_data[ 'say_yes' ]
-prompt = "Ask me whether you should buy or sell Bitcoin. \n\n"
+prompt = "Ask me whether you should buy or sell Bitcoin."
+alt_prompt = "Next question"
 
-print prompt
+def print_f( str='' )
+  print "\n > ", str, "\n\n"
+end
+
+def rand_resp( k )
+   resps = @language_data[ k.to_s ]
+
+   resps[ rand( resps.length ) ]
+end
+
+print_f prompt
+
+ques_num = 0
 
 until ( user_input = gets.chomp.strip ) =~ /(?:ex|qu)it/ix
-	input_is_ques = user_input[ -1 ].eql? '?'
+  ques_num +=1
 
-  if user_input =~ /^.*(sell.*bit\ coin|sell.*bitcoin).*$/ix && input_is_ques
-  	puts
-    puts "#{ say_no[ rand( say_no.length )]}, you should NOT sell Bitcoin. You should buy bitcoin."
-  elsif user_input =~ /^.*(buy.*bit\ coin|buy.*bitcoin).*$/ && input_is_ques
-  	puts
-    puts "#{ say_yes[ rand( say_yes.length ) ] }, you should definitely buy Bitcoin."
+  if ( user_input[ -1 ] != '?' )
+    print_f "Please ask a question ending with a question mark \'?\'"
+
+  elsif user_input =~ /^.*((buy.*or.*sell.*(bit\ coin|btc|bitcoin))|(sell.*or.*buy.*(bit\ coin|btc|bitcoin))).*$/ix
+    print_f "#{ rand_resp( 'bullish_phrases' ) }, you should definitely buy some BTC."
+
+  elsif user_input =~ /^.*(sell.*(bit\ coin|btc|bitcoin)).*$/ix
+    print_f "#{ rand_resp( 'say_no' ) }, you should NOT sell Bitcoin. You should buy Bitcoin."
+  elsif user_input =~ /^.*(buy.*(bit\ coin|btc|bitcoin)).*$/ix
+    print_f "#{ rand_resp( 'say_yes' ) }, you should definitely buy Bitcoin."
   else
-  	puts
-    puts "Please ask a question about buying or selling Bitcoin, and end it with '?'"
+    print_f "Please ask a question about buying or selling Bitcoin."
   end
-  print prompt
+  sleep 0.5
+  print_f ( ques_num.even? ? prompt : 'Next question.' )
 end
 
